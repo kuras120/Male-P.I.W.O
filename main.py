19e5c8876008d73a9a9cd11d0edb6bab21a44e4f
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2019-11-08 20:43:59 +0100

    Add instructions for connecting to bluetooth speaker

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-from samplebase import SampleBase
+#!/usr/bin/env python3
+from matrix.bindings.python.samples.samplebase import SampleBase
 import time
 from random import randrange
 
@@ -13,9 +13,9 @@ class SimpleSquare(SampleBase):
         while True:
             frames = self.get_frames(offset_canvas)
             for frame in frames:
-                self.drawFrame(offset_canvas, frame["matrix"], frame["time"])
+                self.draw_frame(offset_canvas, frame["matrix"], frame["time"])
 
-    def drawFrame(self, offset_canvas, matrix, period_time):
+    def draw_frame(self, offset_canvas, matrix, period_time):
         for y in range(0, offset_canvas.height):
             for x in range(0, self.matrix.width):
                 offset_canvas.SetPixel(
@@ -30,20 +30,16 @@ class SimpleSquare(SampleBase):
             for y in range(0, offset_canvas.height):
                 matrix.append([])
                 for x in range(0, offset_canvas.width):
-                    led = {}
-                    led["r"] = randrange(256)
-                    led["g"] = randrange(256)
-                    led["b"] = randrange(256)
+                    led = {"r": randrange(256), "g": randrange(256), "b": randrange(256)}
                     matrix[y].append(led)
-            frame = {}
-            frame["time"] = 5
-            frame["matrix"] = matrix
+            frame = {"time": 5, "matrix": matrix}
             frames.append(frame)
         return frames
 
-
     # Main function
+
+
 if __name__ == "__main__":
     simple_square = SimpleSquare()
-    if (not simple_square.process()):
+    if not simple_square.process():
         simple_square.print_help()
